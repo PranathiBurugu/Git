df85f1dc994e6cdc7784881fec0ef8af98e944f5
PranathiBurugu <burugupranathi121@gmail.com>
2025-03-20 10:06:19 +0530

    git demo

---
+++ uiDemo/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { Box } from '@mui/material';
+
+function App() {
+  return (
+    <div>
+      <Navbar bg='dark' data-bs-theme='dark'>
+        <Container>
+          <Navbar.Brand>PRANATHI</Navbar.Brand>
+          <Nav>
+            <Nav.Link href=''>Home</Nav.Link>
+            <Nav.Link href=''>About</Nav.Link>
+            <Nav.Link href=''>Projects</Nav.Link>
+            <Nav.Link href=''>Contact</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <Box sx={{minwidth}}>
+      <card variant='outlined'>
+        <CardContent>
+          This is my card
+        </CardContent>
+      </card>
+      </Box>
+    </div>
+  )
+}
+
+export default App
